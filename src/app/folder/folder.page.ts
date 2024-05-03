346959faee1f788b96c9b659bd54e145f7ca7bd8
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-05-03 15:43:25 -0300

    primeiro commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,10 +8,53 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FolderPage implements OnInit {
   public folder!: string;
-  private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  private count = 0;
+  private scoreElement!: HTMLElement;
 
+  constructor(private activatedRoute: ActivatedRoute, private elementRef: ElementRef<HTMLElement>) {}
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.scoreElement = this.elementRef.nativeElement.querySelector('.score-texto') ?? this.elementRef.nativeElement;
+
+    const mario = document.querySelector('.mario') as HTMLImageElement;
+    const pipe = document.querySelector('.pipe') as HTMLElement;
+    
+    const jump = () => {
+
+    
+      mario.classList.add('jump');
+      setTimeout(() => {
+        mario.classList.remove('jump');
+      }, 500);
+    };
+    // const jump = () => {
+    //   mario.classList.add('jump');
+    //   setTimeout(() => {
+    //     mario.classList.remove('jump');
+    //   }, 500);
+    // };
+    document.addEventListener('keydown', jump );
+    
+    
+    const checkCollision = () => {
+      const pipePosition = pipe.offsetLeft;
+      const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+      if (pipePosition <= 50 && marioPosition > 0 && marioPosition < 80) {
+        // Game over
+        mario.src = "/assets/img/game-over.png";
+        mario.style.width = '70px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+      }
+    };
+
+    const loop = setInterval(checkCollision, 1);
+  }
+
+  down(): void {
+    this.count++;
+    this.scoreElement.innerText = `${this.count}`;
   }
 }
